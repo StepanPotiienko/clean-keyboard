cad17d2210e964e2ba8ac9d89cc67c47b120a359
Stepan Potiienko <kotnexjieb@protonmail.com>
2025-02-20 20:19:30 +0200

    implemented gui

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import customtkinter as ctk
 from pynput.keyboard import Listener
 import notifypy
+
+
+def activate_cleaning_mode():
+    # TODO: #1 App crashes upon activating the cleaning mode.
+    with Listener(on_press=on_press) as listener:
+        send_notification("Cleaning mode activated.", "SWEEP, SWEEP, SWEEP")
+        listener.join()
 
 
 def send_notification(title: str, message: str):
@@ -15,6 +23,16 @@ def on_press(key):
     return key
 
 
-with Listener(on_press=on_press) as listener:
-    send_notification("Cleaning mode activated.", "SWEEP, SWEEP, SWEEP")
-    listener.join()
+root = ctk.CTk()
+root.geometry("640x480")
+root.title("Sweeper")
+root.iconbitmap("icon.ico")
+
+root.resizable(False, False)
+root.maxsize(640, 480)
+root.minsize(640, 480)
+
+start = ctk.CTkButton(root, text="Activate", command=activate_cleaning_mode)
+start.pack()
+
+root.mainloop()
